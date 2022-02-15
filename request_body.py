c33b2ff85515f2930f1652c19bed3a8e529a7c6e
Fabian Thorsen <thorsen.fabian@gmail.com>
2022-02-15 19:32:54 +0100

    post and put verbs

---
+++ request_body.py
@@ -0,0 +1,39 @@
+from typing import Optional
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from uuid import uuid4
+
+# To send data from our client/browser to the API, we use the request body
+# We use the HTTP verb /POST to send things to our API
+
+
+class Item(BaseModel):
+    item_id: str = str(uuid4())
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
+
+# Models are classes that inherit from the BaseModel class in pydantic
+
+app = FastAPI()
+
+
+@app.post("/items/")
+async def create_item(
+    item: Item,
+):  # The input is of type Item, so it needs to atleast have a: name and price
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update(
+            {"price_with_tax": price_with_tax}
+        )  # Update the item_dict with the new attribute
+    return item_dict
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: str, item: Item):
+    return {"item_id": item_id, **item.dict()}
